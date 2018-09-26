6975e4edc4b3f6a1ac0dd24de3b205fd49199ab1
Hope Tambala <hopetambala@outlook.com>
2018-09-25 20:58:11 -0400

    Initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hopetambala/Documents/development/school/django_unesco/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
